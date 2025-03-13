d9a1dc2745e7ab5459fb82d083ae6377537bfea8
DimaDmitrii <d.deshura@yandex.ru>
2025-03-13 12:20:18 +0300

    Навигация фрагментов работает

--- QuestCityProject/app/src/main/java/com/example/questcityproject/ui/MainScreenActivity.kt
+++ QuestCityProject/app/src/main/java/com/example/questcityproject/ui/MainScreenActivity.kt
@@ -3,19 +3,30 @@ package com.example.questcityproject.ui
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.questcityproject.R
+import com.example.questcityproject.databinding.MainScreenActivityBinding
+import com.example.questcityproject.ui.notifications.NotificationsFragment
+import com.example.questcityproject.ui.profile.ProfileFragment
+import com.example.questcityproject.ui.quest.list.QuestListFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainScreenActivity : AppCompatActivity() {
+    private lateinit var binding: MainScreenActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_screen_activity)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        binding = MainScreenActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+//        setContentView(R.layout.main_screen_activity)
+//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -23,19 +34,8 @@ class MainScreenActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home,
             R.id.navigation_dashboard,
-            R.id.navigation_notifications,))
+            R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-//        val menu_quests_list_button : Button = findViewById(R.id.menu_quests)
-
-
-//        enterButton.setOnClickListener {
-//            goToEnterPage()
-//        }
-
-//        registrationButton.setOnClickListener {
-//            goToRegistrationPage()
-//        }
     }
 }
--- QuestCityProject/app/src/main/java/com/example/questcityproject/ui/quest/list/QuestListViewModel.kt
+++ QuestCityProject/app/src/main/java/com/example/questcityproject/ui/quest/list/QuestListViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class QuestListViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is quest list Fragment"
     }
     val text: LiveData<String> = _text
 }
