56471e50fa88423295110e1ebf33f2857384cc84
DimaDmitrii <d.deshura@yandex.ru>
2025-03-16 21:32:39 +0300

    Баги на экране списка квестов исправлены

--- QuestCityProject/app/src/main/java/com/example/questcityproject/ui/quest/list/QuestListFragment.kt
+++ QuestCityProject/app/src/main/java/com/example/questcityproject/ui/quest/list/QuestListFragment.kt
@@ -25,7 +25,7 @@ class QuestListFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    private var questsList: MutableList<QuestListBar> = mutableListOf()
+    private var questsList: List<QuestListBar> = listOf()
 
     private var noQuestsField: TextView? = null
     private var isActiveQuestsChecked: Boolean = false
@@ -41,20 +41,22 @@ class QuestListFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.fragment_quest_list, container, false)
 
+        var listArg = mutableListOf<QuestListBar>()
         // Инициализация списка элементов
-        questsList.add(QuestListBar(0, "Булочная №X", "Посети все пекарни Ф.Вольчека", false, 99))
-        questsList.add(QuestListBar(1, "Город мостов", "Посети все мосты Петербурга", false, 465))
-        questsList.add(QuestListBar(2, "Общежития ЛЭТИ", "Посети все общежития ЛЭТИ", false, 9))
-        questsList.add(QuestListBar(3, "Фрукты в плитке", "Найди все арт-объекты в тратуарной плитке", false, 70))
-        questsList.add(QuestListBar(4, "Булочка с корицей", "Посети памятник булочке с корицей", false, 1))
-        questsList.add(QuestListBar(5, "Хлебобулочный комбинат", "Посети хлебобулочный комбинат", false, 1))
-        questsList.add(QuestListBar(6, "Хлебобулочный комбинат", "Посети хлебобулочный комбинат", true, 1, 0))
-        questsList.add(QuestListBar(7, "Хлебобулочный комбинат", "Посети хлебобулочный комбинат", true, 15, 3))
-        questsList.add(QuestListBar(8, "Город мостов", "Посети все мосты Петербурга", true, 465, 279))
-        questsList.add(QuestListBar(9, "Общежития ЛЭТИ", "Посети все общежития ЛЭТИ", true, 9, 1))
-        questsList.add(QuestListBar(10, "Булочка с корицей", "Посети памятник булочке с корицей", false, 1))
-        questsList.add(QuestListBar(11, "Фрукты в плитке", "Найди все арт-объекты в тратуарной плитке", false, 96))
-        questsList.add(QuestListBar(12, "Фрукты в плитке", "Найди все арт-объекты в тратуарной плитке", true, 96, 12))
+        listArg.add(QuestListBar(0, "Булочная №X", "Посети все пекарни Ф.Вольчека", false, 99))
+        listArg.add(QuestListBar(1, "Город мостов", "Посети все мосты Петербурга", false, 465))
+        listArg.add(QuestListBar(2, "Общежития ЛЭТИ", "Посети все общежития ЛЭТИ", false, 9))
+        listArg.add(QuestListBar(3, "Фрукты в плитке", "Найди все арт-объекты в тратуарной плитке", false, 70))
+        listArg.add(QuestListBar(4, "Булочка с корицей", "Посети памятник булочке с корицей", false, 1))
+        listArg.add(QuestListBar(5, "Хлебобулочный комбинат", "Посети хлебобулочный комбинат", false, 1))
+        listArg.add(QuestListBar(6, "Хлебобулочный комбинат", "Посети хлебобулочный комбинат", true, 1, 0))
+        listArg.add(QuestListBar(7, "Хлебобулочный комбинат", "Посети хлебобулочный комбинат", true, 15, 3))
+        listArg.add(QuestListBar(8, "Город мостов", "Посети все мосты Петербурга", true, 465, 279))
+        listArg.add(QuestListBar(9, "Общежития ЛЭТИ", "Посети все общежития ЛЭТИ", true, 9, 1))
+        listArg.add(QuestListBar(10, "Булочка с корицей", "Посети памятник булочке с корицей", false, 1))
+        listArg.add(QuestListBar(11, "Фрукты в плитке", "Найди все арт-объекты в тратуарной плитке", false, 96))
+        listArg.add(QuestListBar(12, "Фрукты в плитке", "Найди все арт-объекты в тратуарной плитке", true, 96, 12))
+        questsList = listArg as List<QuestListBar>
 
         recyclerView = view.findViewById(R.id.questList)
         recyclerView?.layoutManager = LinearLayoutManager(context)
@@ -62,24 +64,16 @@ class QuestListFragment : Fragment() {
         drawQuestsListPart(questsList)
 
         noQuestsField = view.findViewById(R.id.noQuestsLabel)
-//        val recyclerView : RecyclerView = view.findViewById(R.id.questList)
-//        val adapter = ListAdapter(itemList)
-//        recyclerView.layoutManager = LinearLayoutManager(context)
-//        recyclerView.adapter = adapter
-//
-//        adapter.onItemClick = {
-//            Navigation.findNavController(view).navigate(R.id.action_navigation_home_to_questElementFragment);
-//
-//        }
 
 
         val checkboxView: CheckBox = view.findViewById(R.id.activeQuestsCheckbox)
         checkboxView.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
                 isActiveQuestsChecked = true
-                drawQuestsListPart(questsList.filter {
+                val filteredList = questsList.filter {
                     item -> item.isActive
-                })
+                }
+                drawQuestsListPart(filteredList)
             } else {
                 isActiveQuestsChecked = false
                 drawQuestsListPart(questsList)
@@ -90,7 +84,10 @@ class QuestListFragment : Fragment() {
     }
 
     private fun sortQuests() {
-        questsList = questsList.sortedBy { item -> !item.isActive } as MutableList<QuestListBar>
+        if (questsList.isEmpty()) return
+        questsList = questsList.sortedBy {
+            item -> !item.isActive
+        } as MutableList<QuestListBar>
     }
 
     private fun prepareQuestList() {
@@ -98,17 +95,14 @@ class QuestListFragment : Fragment() {
     }
 
     private fun drawQuestsListPart(drawableList: List<QuestListBar>) {
-        if (drawableList.isEmpty() && isActiveQuestsChecked) {
+        if (drawableList.isEmpty()) {
             noQuestsField?.visibility = View.VISIBLE
             noQuestsField?.text = "На данный момент у вас нет активных квестов. Вы можете сделать квест активным, открыв карточку квеста и нажав \"Начать квест\"."
-        } else if (drawableList.isEmpty() && !isActiveQuestsChecked) {
-            noQuestsField?.visibility = View.VISIBLE
-            noQuestsField?.text = "Квесты не найдены"
         } else {
             noQuestsField?.visibility = View.GONE
-            adapter = QuestListAdapter(drawableList)
-            recyclerView?.adapter = adapter
         }
+        adapter = QuestListAdapter(drawableList)
+        recyclerView?.adapter = adapter
     }
 
     override fun onDestroyView() {
