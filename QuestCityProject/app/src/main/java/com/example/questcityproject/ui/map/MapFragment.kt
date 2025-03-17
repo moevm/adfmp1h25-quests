9e5c1d1ae8d17ef2aba1f58fa8d43d72cd375730
Artem Efremov <Atonimaurus@gmail.com>
2025-03-17 11:37:40 +0300

    геометка и навигация

--- QuestCityProject/app/src/main/java/com/example/questcityproject/ui/map/MapFragment.kt
+++ QuestCityProject/app/src/main/java/com/example/questcityproject/ui/map/MapFragment.kt
@@ -1,28 +1,44 @@
 package com.example.questcityproject.ui.map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.questcityproject.R
+import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MapFragment : Fragment() {
 
     private lateinit var mapView: MapView
+    private lateinit var zoomInButton: Button
+    private lateinit var zoomOutButton: Button
+    private lateinit var geoButton: Button
+    private lateinit var myLocationOverlay: MyLocationNewOverlay
+    private lateinit var locationMarker: Marker
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Инициализация OSMDroid
-        org.osmdroid.config.Configuration.getInstance().userAgentValue = requireContext().packageName
+        Configuration.getInstance().userAgentValue = requireContext().packageName
 
         // Загрузка макета
         val view = inflater.inflate(R.layout.fragment_map, container, false)
@@ -38,20 +54,16 @@ class MapFragment : Fragment() {
         mapView.controller.setCenter(startPoint)
 
         // Добавление маркера
-        val marker = Marker(mapView)
+//        val marker = Marker(mapView)
 //        marker.position = startPoint
 //        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
 //        marker.title = "Санкт-Петербург"
 //        mapView.overlays.add(marker)
 
-        // Локация
-        val locationOverlay = MyLocationNewOverlay(mapView)
-        locationOverlay.enableMyLocation()
-        mapView.overlays.add(locationOverlay)
-
         // Инициализация кнопок
-        val zoomInButton: Button = view.findViewById(R.id.zoomInButton)
-        val zoomOutButton: Button = view.findViewById(R.id.zoomOutButton)
+        zoomInButton = view.findViewById(R.id.zoomInButton)
+        zoomOutButton = view.findViewById(R.id.zoomOutButton)
+//        geoButton = view.findViewById(R.id.geoButton)
 
         // Обработка нажатий на кнопки
         zoomInButton.setOnClickListener {
@@ -62,16 +74,63 @@ class MapFragment : Fragment() {
             mapView.controller.zoomOut() // Уменьшаем масштаб
         }
 
+//        geoButton.setOnClickListener {
+//            mapView.controller.animateTo(myLocationOverlay.myLocation)
+//        }
+
+        // Инициализация MyLocationNewOverlay
+        myLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(requireContext()), mapView)
+        myLocationOverlay.enableMyLocation() // Включаем отображение текущего местоположения
+
+
+        // Обновление позиции маркера при изменении местоположения
+        myLocationOverlay.runOnFirstFix {
+            requireActivity().runOnUiThread {
+                mapView.controller.animateTo(myLocationOverlay.myLocation)
+            }
+        }
+
+        // Добавляем наложение на карту
+        mapView.overlays.add(myLocationOverlay)
+
+        // Запрашиваем разрешения на доступ к геолокации
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        }
+
         return view
     }
 
     override fun onResume() {
         super.onResume()
         mapView.onResume() // Обязательно вызывайте onResume для MapView
+        myLocationOverlay.enableMyLocation() // Включаем отображение местоположения при возобновлении
     }
 
     override fun onPause() {
         super.onPause()
         mapView.onPause() // Обязательно вызывайте onPause для MapView
+        myLocationOverlay.disableMyLocation() // Отключаем отображение местоположения при паузе
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Разрешение предоставлено, включаем отображение местоположения
+            myLocationOverlay.enableMyLocation()
+        }
     }
 }
